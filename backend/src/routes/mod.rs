dacd743f31924943bc6ae9a749e9da2679d70c6d
raylandson <www.raylandsoncs@gmail.com>
2025-08-15 00:03:38 -0300

    ✨ feat(notes): adicionar rotas para criar, atualizar e deletar notas

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,5 +1,10 @@
 mod notes;
 
 pub fn get_note_routes() -> Vec<rocket::Route> {
-    routes![notes::create_note, notes::get_all_notes]
+    routes![
+        notes::create_note,
+        notes::get_all_notes,
+        notes::delete_note,
+        notes::update_note
+    ]
 }
