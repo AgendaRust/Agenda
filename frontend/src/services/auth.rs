ba947f089b8a3af9262e9a6cb45e34e570ba312f
raylandson <www.raylandsoncs@gmail.com>
2025-08-20 01:13:26 -0300

    feat: Implement routing
    - Added reqwest and wasm-bindgen-futures dependencies for async operations.
    - Updated main entry point to use the Main component for routing.
    - Refactored Login component to handle username and password input with state management.
    - Integrated async login and note retrieval functionality in the auth service.
    - Created routes for Home and Login pages using yew-router.
    - Styled the login register link in the CSS.

--- frontend/src/services/auth.rs
+++ frontend/src/services/auth.rs
@@ -1,3 +1,15 @@
+use gloo::net::http::Request;
+
 pub fn login() {
     println!("You are logged!");
+}
+
+pub async fn get_notes() -> String {
+    let url = "http://127.0.0.1:8000";
+    let response = Request::get(&url).send().await.unwrap();
+    if response.status() == 200 {
+        format!("notes resulted {}", response.status_text())
+    } else {
+        format!("notes not found {}", response.status_text())
+    }
 }
